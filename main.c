d0075b1d3c4f81855764d9e5ad43bdb2b8bb714f
Anton <ab@air-cloud.org>
2020-10-20 18:52:49 +0300

    threads option

--- main.c
+++ main.c
@@ -7,13 +7,15 @@
 
 int main(int argc, char** argv) {
     srand(time(NULL));
-    int n = 0, block_size = 0;
+    int n = 0, block_size = 0, t = 8;
 
     for (int i = 1; i < argc; i += 2) {
         if (strcmp(argv[i], "-n") == 0) {
             n = atoi(argv[i + 1]);
         } else if (strcmp(argv[i], "-r") == 0) {
             block_size = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-t") == 0) {
+            t = atoi(argv[i + 1]);
         } else {
             return 1;
         }
@@ -24,7 +26,7 @@ int main(int argc, char** argv) {
     }
 
     omp_set_dynamic(0);
-    omp_set_num_threads(8);
+    omp_set_num_threads(t);
 
     run(n, block_size);
 
